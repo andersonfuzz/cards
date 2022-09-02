821ae529395b6ebc200123e0f2f99970844bd91d
Anderson Andrade <andersonfuzz@outlook.com>
2022-09-02 18:27:43 -0300

    att

--- script/game.js
+++ script/game.js
@@ -1,34 +1,13 @@
-const card=document.querySelectorAll('.card')
-      card.addEventListener('click',revelCard)
-
     //Revel Card
-
     function revelCard(){
         card.classList.add('revelCard')
     
-        saveCard(target)
+        //saveCard(target)
 
     }
 
-
-    //SaveCard
-    const card01=''
-    const card02=''
-
-    function saveCard(target){
-        if(card01===''){
-            card01=target
-        }else if(card02===''){
-            card02=target
-        }
-        checkCard(card01,card02)
-    }
-
-    //Check Card
-    function checkCard(card01,card02){
-    if(card01===card02){
-        //permanece revelada
-    }else{
-        //virar carta
-    }
+    function newCard(){
+        const card=document.createElement('div')
+        const front=document.createElement('div')
+        const back=document.createElement('div')
     }
