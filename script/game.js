4017650843094e18440605b1bb37f406bf8fa1ba
Anderson Andrade <andersonfuzz@outlook.com>
2022-09-04 11:27:27 -0300

    addComent

--- script/game.js
+++ script/game.js
@@ -1,6 +1,28 @@
 "use strict";
 //Apontando Grig
 const grid = document.querySelector(".grid");
+
+//Criando carta
+function createCard(character) {
+  const card = createElement("div", "card");
+  const front = createElement("div", "face front");
+  const back = createElement("div", "face back");
+  
+  front.style.backgroundImage = `url(../img/${character}.jpg)`;
+  
+  card.appendChild(front);
+  card.appendChild(back);
+  
+  return card;
+}
+
+//Criando elemento e adicionando classes
+function createElement(tag, $class) {
+  const element = document.createElement(tag);
+  element.classList = $class;
+  return element;
+}
+
 //Personagens
 const characters = [
   "00",
@@ -16,36 +38,14 @@ const characters = [
   "010",
   "011",
 ];
-
-//Criando carta
-function createCard(character) {
-  const card = createElement("div", "card");
-  const front = createElement("div", "face front");
-  const back = createElement("div", "face back");
-
-  front.style.backgroundImage = `url(../img/${character}.jpg)`;
-
-  card.appendChild(front);
-  card.appendChild(back);
-
-  return card;
-}
-
-//Criando elemento e adicionando classes
-function createElement(tag, $class) {
-  const element = document.createElement(tag);
-  element.classList = $class;
-  return element;
-}
 //Tabuleiro
 function board() {
   //Dobrando cartas com operador 'spread'
   const doubleCharacters = [...characters, ...characters];
-
   //Embaralhando cartas
-  const mix = doubleCharacters.sort(() => Math.random() - 0.5);
-  mix.forEach((item) => {
-    const avatar = createCard(item);
+  const mixCharacters = doubleCharacters.sort(() => Math.random() - 0.5);//array.sort()=> Ordena array em ordem alfabética.Math.random retorna um numéro entre 0 e 1,mas sempre menor que um.O '-0.5' garente um número maior ou menor que 0. Vide Mdn
+  mixCharacters.forEach((character) => {
+    const avatar = createCard(character);
     grid.appendChild(avatar);
   });
 }
